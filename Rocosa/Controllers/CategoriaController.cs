41a70d7d2a5c701f52fc80a42a1a580b6eb525c8
Juan Pablo Correa Tarazona <89850618+JuanPCT@users.noreply.github.com>
2023-08-29 21:21:57 -0500

    Se agrega modelo vista y controlador Categoria con form para agregar nueva categoria

---
+++ Rocosa/Controllers/CategoriaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocosa.Data;
+using Rocosa.Models;
+
+namespace Rocosa.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoriaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Categoria> objList = _db.Categoria;
+
+            return View(objList);
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+    }
+}
